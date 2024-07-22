618044a69052ed03c1b69fe7858e3a756ca26d77
GreatDevvie <greatdevvie@gmail.com>
2024-07-22 16:09:52 +0700

    first commit

---
+++ src/data/db.ts
@@ -0,0 +1,23 @@
+import Dexie, { type EntityTable } from 'dexie';
+
+interface Nodes {
+  id: number;
+  time: string;
+  header: string;
+  body: React.SetStateAction<string>;
+}
+
+const db = new Dexie('NodeDatabase') as Dexie & {
+  notes: EntityTable<
+    Nodes,
+    'id' 
+  >;
+};
+
+// Schema declaration:
+db.version(1).stores({
+  notes: '++id, time, header, body'
+});
+
+export type { Nodes };
+export { db };
